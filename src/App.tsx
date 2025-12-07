9e7247c8e199edbf262105967365a30110611e5b
Alex Reiffer <awr7648@gmail.com>
2025-12-07 17:50:01 -0500

    Initial repo for step 1 from auto template

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Alex Reiffer</h1>
+      <p className="blurb">
+        I am Alex Reiffer, a student in Computer Science at Grand Valley State University. In my free time I enjoy either gaming or playing one of my many instruments. I also enjoy being physically active and will go on long bike rides. 
+      </p>
+    </>
+  )
+}
+
+export default App
