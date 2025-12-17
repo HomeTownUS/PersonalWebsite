864859a4a7bbad966c42360a2b1cb4f5f7bd5bb4
Alex Reiffer <awr7648@gmail.com>
2025-12-17 13:42:56 -0500

    Changed styling to tailwindcss and changed ui style

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './index.css'
 
 function App() {
   return (
     <>
-      <h1>Alex Reiffer</h1>
-      <p className="blurb">
-        I am Alex Reiffer, a student in Computer Science at Grand Valley State University. In my free time I enjoy either gaming or playing one of my many instruments. I also enjoy being physically active and will go on long bike rides. 
-      </p>
+      <div className="flex flex-col items-center justify-center text-center px-4">
+        <h1 className="text-4xl font-italic mb-4">Alex Reiffer</h1>
+        <p className="blurb max-w-lg">
+          I am Alex Reiffer, a student in Computer Science at Grand Valley State University. 
+          In my free time I enjoy either gaming or playing one of my many instruments. 
+          I also enjoy being physically active and will go on long bike rides. 
+        </p>
+      </div>
     </>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as path from 'path'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    tailwindcss(),
+    react(),
+  ],
+  resolve: {
+    alias: {
+      // The alias must map to an ABSOLUTE path
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
